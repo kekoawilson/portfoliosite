2fd7454b7c8f6f7d53dc230e1ba6d0269b487b47
Kekoa Wilson <kekoawilson20@gmail.com>
2018-12-10 22:16:12 -0700

    added computers, landscape.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.use( cors() )
 
 app.use( express.static( `${__dirname}/..` ) );
 app.use( express.static( `${__dirname}/images` ) );
+app.use( express.static( `${__dirname}/images/computers` ) );
+app.use( express.static( `${__dirname}/images/landscape` ) );
 app.use( express.static( `${__dirname}/assets` ) );
 
 app.listen(process.env.SERVER_PORT, () =>console.log(`listening on port: ${process.env.SERVER_PORT}`))
