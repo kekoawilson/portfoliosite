d16c50d5b1cf9a4471ece7d62978a7cc30f26c33
Kekoa Wilson <kekoawilson20@gmail.com>
2018-12-10 21:32:50 -0700

    added static files for faster rendering.

--- server/index.js
+++ server/index.js
@@ -6,5 +6,7 @@ const app = express()
 app.use( cors() )
 
 app.use( express.static( `${__dirname}/..` ) );
+app.use( express.static( `images` ) );
+app.use( express.static( `assets` ) );
 
 app.listen(process.env.SERVER_PORT, () =>console.log(`listening on port: ${process.env.SERVER_PORT}`))
